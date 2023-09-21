ba78d4d35baa88ded0eea17591993d156fd9cd13
xeth hung <adamhung12@gmail.com>
2023-09-21 16:39:30 +0800

    add test case

--- src/cmdu/lines/cmd_set_nu.py
+++ src/cmdu/lines/cmd_set_nu.py
@@ -5,3 +5,10 @@ def cmd_set_nu(ins, out):
     appender = LineNumberAppender()
     for l in ins:
         out.write(appender.append(l))
+
+
+def cmd_set_split(ins, out, delimiter):
+    for l in ins:
+        ls = l.rstrip("\n").split(delimiter)
+        for x in ls:
+            out.write(f"{x}\n")
--- src/cmdu/test/test_line/test_line.py
+++ src/cmdu/test/test_line/test_line.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
 
-from cmdu.lines.cmd_set_nu import cmd_set_nu
+from cmdu.lines.cmd_set_nu import cmd_set_nu, cmd_set_split
 
 
-class Writer():
+class Writer:
     def __init__(self):
         self.lines = []
 
@@ -21,3 +21,15 @@ class TestLine(TestCase):
         self.assertEqual(write[0], "000001 |a")
         self.assertEqual(write[1], "000002 |b")
         self.assertEqual(write[2], "000003 |c")
+
+    def test_split(self):
+        writer = Writer()
+        test_input = ["a|x", "bx", "c|x"]
+        cmd_set_split(test_input, writer, "|")
+        write = writer.lines
+        self.assertEqual(len(write), 5)
+        self.assertEqual(write[0], "a\n")
+        self.assertEqual(write[1], "x\n")
+        self.assertEqual(write[2], "bx\n")
+        self.assertEqual(write[3], "c\n")
+        self.assertEqual(write[4], "x\n")
